00456bdf4f03a037fc5bbe83ecd32d96ec2a3f05
Thong Le <thongle9.27@gmail.com>
2017-06-08 01:41:11 -0700

    update 10 and 44

--- python/_010_regular_expression_matching.py
+++ python/_010_regular_expression_matching.py
@@ -1,9 +1,41 @@
 # Author: Thong Le
-# Date: 
+# Date: June 07, 2017
 #
 # LeetCode 10 - Regular Expression Matching
 #
-# Solution Approach: 
+# Solution Approach: Dynamic Programming (Topdown approach)
 #
 #
 #
+
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        dp = {}
+        return self.topdown(s, p, 0, 0, dp)
+    
+    def topdown(self, s, p, i, j, dp):
+        if (i == len(s) and j == len(p)):
+            return True
+        if (j >= len(p)):
+            return False
+        if (i,j) in dp:
+            return dp[(i,j)]
+        if (j+1 < len(p)) and (p[j+1] == '*'):
+            b1 = self.topdown(s, p, i, j+2, dp)
+            if i < len(s) and (p[j] == '.' or p[j] == s[i]):
+                b2 = self.topdown(s, p, i+1, j, dp)
+                dp[(i,j)] = b1 or b2
+            else:
+                dp[(i,j)] = b1
+        else:
+            if i < len(s) and (p[j] == '.' or p[j] == s[i]):
+                dp[(i,j)] = self.topdown(s, p, i+1, j+1, dp)
+            else:
+                dp[(i,j)] = False
+        return dp[(i,j)]
+        
--- python/_044_wildcard_matching.py
+++ python/_044_wildcard_matching.py
@@ -1,9 +1,24 @@
 # Author: Thong Le
-# Date: 
+# Date: June 08, 2017
 #
 # LeetCode 44 - Wildcard Matching
 #
-# Solution Approach: 
+# Solution Approach: DP
 #
 #
 #
+
+class Solution(object):
+    
+    def isMatch(self, s, p):
+        n = len(s)
+        dp = [True] + [False]*n
+        for i in p:
+            if i != '*':
+                for j in reversed(range(n)):
+                    dp[j+1] = dp[j] and (i == s[j] or i == '?')
+            else:
+                for j in range(1, n+1):
+                    dp[j] = dp[j-1] or dp[j]
+            dp[0] = dp[0] and i == '*'
+        return dp[-1]
